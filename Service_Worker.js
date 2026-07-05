0b06ac4bc5218a74ade1b9ce9053782c237c5def
Abdullah As-Sadeed <abdullah_as-sadeed.bd@protonmail.com>
2026-07-05 23:09:52 +0600

    Added Service Worker with Offline Fallback.

---
+++ Service_Worker.js
@@ -0,0 +1,55 @@
+/* By Abdullah As-Sadeed */
+
+"use strict";
+
+const cacheName = "Bitscoper_Computer_Museum";
+const offlineFallbackPage = "Offline_Fallback.txt";
+
+self.onmessage = function (event) {
+  if (event.data && event.data?.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+};
+
+self.addEventListener("install", async function (event) {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.add(offlineFallbackPage)),
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    (async () => {
+      if ("navigationPreload" in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
+
+      await self.clients.claim();
+    })(),
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.mode == "navigate") {
+    event.respondWith(
+      (async function () {
+        try {
+          const preloadResponse = await event.preloadResponse;
+
+          if (preloadResponse) {
+            return preloadResponse;
+          } else {
+            const networkResponse = await fetch(event.request);
+
+            return networkResponse;
+          }
+        } catch (error) {
+          const cache = await caches.open(cacheName);
+          const cachedResponse = await cache.match(offlineFallbackPage);
+
+          return cachedResponse;
+        }
+      })(),
+    );
+  }
+});
